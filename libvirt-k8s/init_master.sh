85f73608ee6350914d375444a0dece9c2a07b10c
Marek Ruszczyk <kirek007@gmail.com>
2020-02-17 20:00:55 +0100

    [WIP] Vagrant + Libvirt + k8s

---
+++ libvirt-k8s/init_master.sh
@@ -0,0 +1,15 @@
+##!/usr/bin/env bash
+#
+#kubeadm init --kubernetes-version="${KUBERNETES_VERSION}" --apiserver-advertise-address="${MASTER_IP}" --apiserver-cert-extra-sans="${MASTER_IP}" --token "${KUBERNETES_TOKEN}" --token-ttl 120m --pod-network-cidr=10.72.0.0/16
+#
+#export KUBECONFIG=/etc/kubernetes/admin.conf
+#kubectl apply -f https://docs.projectcalico.org/v3.9/manifests/calico.yaml
+#
+#systemctl restart kubelet
+#
+## Prepare config
+#export HOME=/home/vagrant
+#mkdir -p $HOME/.kube
+#sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+#sudo chown "$(id vagrant -u)":"$(id vagrant -g)" $HOME/.kube/config
+#sudo cp $HOME/.kube/config /vagrant
